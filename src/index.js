4746078a8e639e84dedae6d6a33bacf6f5029112
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2026-01-24 21:59:30 -0300

    Remade UI style, added projects list and made to-do creator.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
 import "./styles.css";
-
-import { CreateProjectPrompt, CreateProjects, ClearProjects } from "./DOMhandler";
-
-const btnNewProject = document.getElementById("btnNewProject");;
-
-btnNewProject.addEventListener('click', () => {
-    ClearProjects();
-    CreateProjects();
-    CreateProjectPrompt();
-});
-
-document.addEventListener('click', e => {
-    if (e.target.id == "project") {
-
-    };
-}); 
-
-CreateProjects();
